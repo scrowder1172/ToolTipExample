9cd9842df8d9074bf2a7acf1125fef4909b37573
Scott Crowder <scrowder@gmail.com>
2025-01-08 14:24:20 -0600

    add files

---
+++ ToolTipExample/Tooltip.swift
@@ -0,0 +1,21 @@
+//
+// File: Tooltip.swift
+// Project: ToolTipExample
+// 
+// Created by SCOTT CROWDER on 1/8/25.
+// 
+// Copyright © Playful Logic Studios, LLC 2025. All rights reserved.
+// 
+
+
+import SwiftUI
+
+struct Tooltip: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Tooltip()
+}
---
+++ ToolTipExample/TooltipModel.swift
@@ -0,0 +1,11 @@
+//
+// File: TooltipModel.swift
+// Project: ToolTipExample
+// 
+// Created by SCOTT CROWDER on 1/8/25.
+// 
+// Copyright © Playful Logic Studios, LLC 2025. All rights reserved.
+// 
+
+
+import Foundation
