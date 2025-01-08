c5f4953fdd70b569d7186671bd740dc7dd0de210
Scott Crowder <scrowder@gmail.com>
2025-01-08 11:26:29 -0600

    Initial Commit

---
+++ ToolTipExample/ToolTipExampleApp.swift
@@ -0,0 +1,20 @@
+//
+// File: ToolTipExampleApp.swift
+// Project: ToolTipExample
+// 
+// Created by SCOTT CROWDER on 1/8/25.
+// 
+// Copyright © Playful Logic Studios, LLC 2025. All rights reserved.
+// 
+
+
+import SwiftUI
+
+@main
+struct ToolTipExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
